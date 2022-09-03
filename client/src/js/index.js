6e23c3aed41143a054f908a140666f03ccdc5400
jbradley84 <utbradley84@yahoo.com>
2022-09-03 16:13:31 -0500

    updated start script in root package.json

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,11 +10,13 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
-// add images on load
-window.addEventListener('load', function () {
-   document.getElementById('logo').src = Logo;
-   document.getElementById('bearThumbnail').src = Bear;
-   document.getElementById('dogThumbnail').src = Dog;
+import { initdb } from './database';
+
+window.addEventListener('load', function() {
+  initdb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
 });
 
 // Import CSS files
